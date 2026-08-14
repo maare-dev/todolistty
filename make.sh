d760b62c7145accb85c2f44f088a4087fa6addb5
maare-dev <aleksandr.homewrk@gmail.com>
2026-08-14 16:48:25 +0300

    initial commit

---
+++ make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source=(
+)
+flags=(
+# here some flags I use
+    "-std=c++20"
+    "-Wall"
+    "-Wextra"
+    "-Wpedantic"
+    "-Werror"
+    "-g"
+)
+libs=(
+)
+oname='proj_0.0.1'
+
+#source filling
+mapfile -d $'\0' source < <(find "$(pwd)/src" -type f -name "*.cpp" -print0)
+
+#compiling project
+g++ "${flags[@]}" -o "$oname" "${source[@]}" "${libs[@]}" || exit 1
+
+#run or no
+run=''
+read -r -p "run? (Y/n)" run
+case "$run" in
+    ""|[Yy])
+        clear
+        "./$oname"
+        ;;
+    *)
+        echo "cancelled"
+        ;;
+esac
